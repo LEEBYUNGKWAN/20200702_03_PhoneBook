1bb7bfdac344613aa5d35f3999e71f9d34fefda6
LEEBYUNGKWAN <67721588+LEEBYUNGKWAN@users.noreply.github.com>
2020-07-02 12:43:46 +0900

    무한으로 계속 입력받는 방법

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,34 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	
 	public static void main(String[] args) {
 		
+		
+		
+		printMenu();
+		
+		public static void printMenu() {
+			
+			
+		}
+		
 //		Git과 연동해서 전화번호부 만들 예정
+		Scanner myScan =  new Scanner(System.in);
+		
+		while (true) {
+			
+			System.out.println("******전화번호부******");
+		    System.out.println("1) 전화번호 추가등록");
+		    System.out.println("2) 전체 전화번호 목록 조회");
+		    System.out.println("0 프로그램 종료.");
+		    System.out.println("====================");
+			
+			System.out.print("메뉴 선택:  ");
+			int inputMenu = myScan.nextInt();
+		}
 	}
 
 }
